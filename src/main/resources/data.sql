3f8668dc3ff81a6dc1096c3848bc1eec4660de5a
MxVndI <kingknife29@gmail.com>
2024-10-22 23:08:44 +0300

    fix bugs

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,19 @@
-INSERT INTO mpa (name) VALUES ('G'), ('PG'), ('PG-13'), ('R'), ('NC-17');
-INSERT INTO genres (name) VALUES ('Комедия'), ('Драма'), ('Мультфильм'), ('Триллер'), ('Документальный'), ('Боевик');
+MERGE INTO mpa AS target
+USING (
+    VALUES ('G'), ('PG'), ('PG-13'), ('R'), ('NC-17')
+) AS source (name)
+ON target.name = source.name
+WHEN MATCHED THEN
+    UPDATE SET name = source.name
+WHEN NOT MATCHED THEN
+    INSERT (name) VALUES (source.name);
+
+MERGE INTO genres AS target
+USING (
+    VALUES ('Комедия'), ('Драма'), ('Мультфильм'), ('Триллер'), ('Документальный'), ('Боевик')
+) AS source (name)
+ON target.name = source.name
+WHEN MATCHED THEN
+    UPDATE SET name = source.name
+WHEN NOT MATCHED THEN
+    INSERT (name) VALUES (source.name);
